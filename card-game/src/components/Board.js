18b461f98aa1ae11d2c0fe491c0ac1b0c04c2fab
Igor-D-art <gorgsemenon1@gmail.com>
2022-07-08 10:40:55 +0300

    Ironed out a few details

--- card-game/src/components/Board.js
+++ card-game/src/components/Board.js
@@ -7,9 +7,7 @@ function Board() {
 
     (function cardPopulater() {
         for (let i = 0; i < 12; i++) {
-            // const card = <img src={`/images/${i+1}.png`} alt=" " />
             const card = {
-                // img: <img id=" " src={`/images/${i + 1}.png`} alt=" " />,
                 link: `/images/${i + 1}.png`,
                 counter: 0,
             }
@@ -24,7 +22,6 @@ function Board() {
     const [bestScore, setBestScore] = useState(0);
 
     const incremCounter = (event, tempCards) => {
-        console.log(event.target)
         // make a shallow copy of the element where the counter needs to be increased
         const tempCard = { ...tempCards[event.target.id] }
         // updating the property
@@ -83,7 +80,6 @@ function Board() {
 
     useEffect(() => {
         console.log('component did update');
-        console.log(cards)
     }, [score])
 
 
